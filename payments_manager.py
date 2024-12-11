cb69da6539cbd583f8b0f469d13d2d39f984740c
cpujol-dev <cescfran17@gmail.com>
2024-12-11 08:44:32 +0100

    Quitamos el -1 de n_backs en la libreria. Tras Identificar casos en los que restar 1 a n_backs nos da un resultado incorrecto: user_id: 34, 35 47 83 163 191 204 213

--- payments_manager.py
+++ payments_manager.py
@@ -693,7 +693,7 @@ class Manager:
         # # Para stat_cr == "money_back" & stat_fe == "accepted" acumulamos el numero de operaciones de tipo money_back        
         df = df.sort_values(['created_at','created_at_fe'])
         unique_cr = (df['stat_cr'] == "money_back") & (df['amount'] > 0) & ~df.duplicated(subset=['id_cr'], keep='first')
-        df['n_backs'] = unique_cr.groupby(df['user_id']).cumsum()-1
+        df['n_backs'] = unique_cr.groupby(df['user_id']).cumsum() # -1 # 2024-12-11 Cesc no podemos hacer el -1 esto nos deja casos en negativo que son claramente erroneos
 
         #df = df.drop(columns=['n_backs'])
         #df['n_backs'] = (df['stat_cr'] == "money_back")  & (df['amount'] > 0)
